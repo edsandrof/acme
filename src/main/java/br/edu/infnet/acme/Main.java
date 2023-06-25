820d512a4b3a78206711ba9a46e9de91ee897223
Edsandro Freitas <edsandrofreitas@gmail.com>
2023-06-25 12:22:06 -0300

    feat: calc subscription fee

--- src/main/java/br/edu/infnet/acme/Main.java
+++ src/main/java/br/edu/infnet/acme/Main.java
@@ -6,7 +6,10 @@ import br.edu.infnet.acme.domain.service.ExerciseList2Service;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.printf(" --- Exercise list 1 --- %n%n");
         ExerciseList1Service.runExerciseList1();
+
+        System.out.printf("%n --- Exercise list 2 --- %n%n");
         ExerciseList2Service.runExerciseList2();
     }
 }
--- src/main/java/br/edu/infnet/acme/domain/service/ExerciseList2Service.java
+++ src/main/java/br/edu/infnet/acme/domain/service/ExerciseList2Service.java
@@ -1,7 +1,21 @@
 package br.edu.infnet.acme.domain.service;
 
+import br.edu.infnet.acme.domain.model.Subscription;
+import br.edu.infnet.acme.infrastructure.factory.SubscriptionFactory;
+
+import java.util.List;
+
 public class ExerciseList2Service {
     public static void runExerciseList2() {
+        List<Subscription> subscriptions = List.of(
+                SubscriptionFactory.getAnnualSubscription(),
+                SubscriptionFactory.getHalfYearSubscription(),
+                SubscriptionFactory.getQuarterlySubscription()
+        );
+
+        subscriptions.stream()
+                .map(s -> String.format("\t> %s's subscription - total cost: %.2f, fee: %.2f", s.getCustomer().getName(), s.getTotalCost(), s.getFee()))
+                .forEach(System.out::println);
 
     }
 }
